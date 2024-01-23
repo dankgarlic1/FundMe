1d691cec74e112ca1d8e5b57f0e5078e43ce6161
Harshit Raizada <raizadaharshit2004@gmail.com>
2024-01-23 19:34:23 +0530

    Setup

---
+++ contracts/FundMe.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.8;
+import "./PriceConverter.sol";
+
+error notOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public constant MINIMUM_USD = 50 * 1e18; //constant variables have different naming convention
+
+    address[] Funders;
+    mapping(address => uint256) addressToAmountFunded;
+
+    address public immutable i_owner; //different naming convention _i for immutable variables
+
+    constructor() {
+        i_owner = msg.sender; //msg.sender here the person who deploys the co tract since it is in Constructor
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "Didn't Send Enough"
+        );
+        // require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough");//1e18 = 1 * 10**28 gwei
+        Funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 fundIndex = 0; fundIndex < Funders.length; fundIndex++) {
+            address funder = Funders[fundIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        Funders = new address[](0);
+        //to withdraw funds we have 3 methods, transfer, send and call
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        bool paymentSuccess = payable(msg.sender).send(address(this).balance);
+        require(paymentSuccess, "FAIL");
+
+        //call(recommended)
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }(""); //returns 2 variable b ut we only need one
+        require(callSuccess, "fail");
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender==i_owner);
+        if (msg.sender != i_owner) {
+            revert notOwner();
+        } // both do same thing, but ths is more gas efficient
+        _;
+    }
+
+    //What if someone sends ETH to the contract address without calling FUND fucntion?
+
+    //solidity has two special functions for this 1)recieve() 2)fallback()
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("hardhat-deploy");
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-gas-reporter");
+require("dotenv");
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.8",
+};
